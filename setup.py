adb5b6dae5358872b5b1a4f1368041d9a8ea302a
Chris Marsh <chrismarsh.c2@gmail.com>
2020-08-11 13:44:20 -0600

    bump version number

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except SKBuildError:
 
 
 setup(name='windmapper',
-      version='1.2.1',
+      version='1.2.2',
       description='Windfield library generation',
       long_description="""
       Generates windfields
