309f983af4bf5d8bf6f6b6295d4e8288880d51b2
Chris Marsh <chrismarsh.c2@gmail.com>
2023-12-01 20:20:34 -0600

    more clean up

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ else:
     from setuptools import setup
 
 args =   {'name': 'windmapper',
-            'version': '2.1.0',
+            'version': '2.1.1',
             'description': 'Windfield library generation',
             'long_description': "Generates windfields",
             'author': 'Chris Marsh',
