c745f028e54aa6029db9e7d2149b6a5793d6012c
Chris Marsh <chrismarsh.c2@gmail.com>
2023-12-13 12:43:17 -0600

    use a windninja that has an rpath

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ else:
     from setuptools import setup
 
 args =   {'name': 'windmapper',
-            'version': '2.1.11',
+            'version': '2.1.12',
             'description': 'Windfield library generation',
             'long_description': "Generates windfields",
             'author': 'Chris Marsh',
