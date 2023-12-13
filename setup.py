449396ce14d30795832dc703c9d42c15d6e86524
Chris Marsh <chrismarsh.c2@gmail.com>
2023-12-13 13:54:13 -0600

    Improve CI testing when mixing pygdal and gdal

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ else:
     from setuptools import setup
 
 args =   {'name': 'windmapper',
-            'version': '2.1.12',
+            'version': '2.1.13',
             'description': 'Windfield library generation',
             'long_description': "Generates windfields",
             'author': 'Chris Marsh',
